daba0f34bf20a1d93fa2d68ef20c3257ca08aa17
Sushil-Tamang2022 <sushiltamang411@gmail.com>
2025-12-26 22:03:52 +0545

    Format Output

---
+++ FormatOutput.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hey!");
+            Console.WriteLine("Hello!");
+
+            //This is a comment
+            /*
+             * this
+             * is
+             * a
+             * multiline
+             * comment
+             */
+
+            Console.WriteLine("\tBro\nCode");
+
+            Console.ReadKey();
+        }
+    }
+}
