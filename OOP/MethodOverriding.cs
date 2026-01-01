41de0aec672286d67fcde7e074ac557014820d6b
Sushil-Tamang2022 <sushiltamang411@gmail.com>
2026-01-01 16:57:14 +0545

    Method Overriding

---
+++ OOP/MethodOverriding.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            //method overriding = provides a new version of a method inherited from a parent class
+            //                    inherited method must be: abstract, virtual, or already overriden
+            //                    Used with ToString(), polymorphism 
+
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            dog.Speak();
+            cat.Speak();
+
+            Console.ReadKey();
+        }
+    }
+    class Animal 
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("The animal goes *brrr*");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The dog goes *woof*");
+        }
+    }
+    class Cat : Animal
+    {
+
+    }
+}
